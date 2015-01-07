086689a6b9cb1a5fd213324915a735493135eefb
Raul Ramirez <raul287986@gmail.com>
2015-01-06 19:26:34 -0800

    I didnt finish my class because i didnt know where you put the $dog1= new dog(); $dog2 = new dog(); at?

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class Athlete {
+    public $firstName = "Lebron";
+    public $lastName = "James";
+    public $weight = "250";
+    public $age = "30";
+    public $gender = "male";
+    
+    function getName(){
+    return"{$this->firstName}" . 
+    "{$this->lastName}";
+    }
+}
+{
+    
+}
